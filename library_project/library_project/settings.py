4f24c5fd043e3582d707547d6628857c63756fd4
anass nagib <anasnajib19@gmail.com>
2024-12-29 16:29:24 +0100

    commit

--- library_project/library_project/settings.py
+++ library_project/library_project/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-_-w*02_v&9xw-i9x-((d_taf(in94p@*j++(=_2^$g&w8e(m9_
 DEBUG = True
 
 ALLOWED_HOSTS = []
-
+:
 
 # Application definition
 
@@ -130,6 +130,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
